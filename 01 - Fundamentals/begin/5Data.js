b8f55f424fd0a837fc662f7b58f65c9b1ad65a20
Israel Kessler <israel-kessler@hotmail.com>
2025-06-03 05:50:37 -0300

    Learning about primitives data types

---
+++ 01 - Fundamentals/begin/5Data.js
@@ -0,0 +1,16 @@
+// In this lesson, we learn about data types in JavaScript, or more specifically, the primitive data types
+
+// Learning how to use and declare variables in JavaScript
+
+// var is most simple way to declare a variable 
+var currentYear = 2025;                       // Integer
+var firstName = "Kessler"                     // String
+var isStudent = false                         // Boolean
+var height = 1.75                             // Float
+var weight = 62.5                             // Float
+var hobbies = ["reading", "gaming", "coding"] // Array
+
+// Using let to declare a variable, let is used for block-scoped variables
+let currentAge = 29                           // Integer
+let isEmployed = true                         // Boolean
+let favoriteColors = ["blue", "green", "red"] // Array
