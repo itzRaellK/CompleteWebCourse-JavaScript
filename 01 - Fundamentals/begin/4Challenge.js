980e64a57c769458210a784cb1815e00bd0f4f08
Israel Kessler <israel-kessler@hotmail.com>
2025-06-03 05:38:01 -0300

    Studies about Code Block

---
+++ 01 - Fundamentals/begin/4Challenge.js
@@ -0,0 +1,20 @@
+// In this lesson, we need to create two differents blocks of code that will be executed
+// one containing the Odd numbers and the other containing the Even numbers
+
+{
+    console.log("Odd Numbers:")
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+    console.log(9)
+}
+
+{
+    console.log("Even numbers:")
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+}
